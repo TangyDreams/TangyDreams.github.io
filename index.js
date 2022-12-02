6cdb222bf4d75762cb43d6548c4c9cc8c44f68f6
TangyDreams <118681496+TangyDreams@users.noreply.github.com>
2022-12-02 06:48:09 -0500

    disc

--- index.js
+++ index.js
@@ -1,52 +1,44 @@
 function Time() {
+  var date = new Date();
 
+  var hour = date.getHours();
 
-    var date = new Date();
+  var minute = date.getMinutes();
 
+  var second = date.getSeconds();
 
-    var hour = date.getHours();
+  var period = "";
 
-    var minute = date.getMinutes();
+  if (hour >= 12) {
+    period = "PM";
+  } else {
+    period = "AM";
+  }
 
-    var second = date.getSeconds();
-
-    var period = "";
-
-
-    if (hour >= 12) {
-        period = "PM";
-    } else {
-        period = "AM";
+  if (hour == 0) {
+    hour = 12;
+  } else {
+    if (hour > 12) {
+      hour = hour - 12;
     }
+  }
 
+  hour = update(hour);
+  minute = update(minute);
+  second = update(second);
 
-    if (hour == 0) {
-        hour = 12;
-    } else {
-        if (hour > 12) {
-            hour = hour - 12;
-        }
-    }
+  document.getElementById("digital-clock").innerText =
+    hour + " : " + minute + " : " + second + " " + period;
 
-
-    hour = update(hour);
-    minute = update(minute);
-    second = update(second);
-
-
-    document.getElementById("digital-clock").innerText = hour + " : " + minute + " : " + second + " " + period;
-
-
-    setTimeout(Time, 1000);
+  setTimeout(Time, 1000);
 }
 
 function update(t) {
-    if (t < 10) {
-        return "0" + t;
-    }
-    else {
-        return t;
-    }
+  if (t < 10) {
+    return "0" + t;
+  } else {
+    return t;
+  }
 }
 
 Time();
